15a2c520d5ff69268463c22d1a63c8992b0b3aa0
Edgar Žigis <tranced.freak@gmail.com>
2019-08-07 11:11:47 +0300

    KEY_VIEW_STATE_BUNDLE make public

--- conductor/src/main/java/com/bluelinelabs/conductor/Controller.java
+++ conductor/src/main/java/com/bluelinelabs/conductor/Controller.java
@@ -56,7 +56,7 @@ public abstract class Controller {
     private static final String KEY_OVERRIDDEN_PUSH_HANDLER = "Controller.overriddenPushHandler";
     private static final String KEY_OVERRIDDEN_POP_HANDLER = "Controller.overriddenPopHandler";
     private static final String KEY_VIEW_STATE_HIERARCHY = "Controller.viewState.hierarchy";
-    static final String KEY_VIEW_STATE_BUNDLE = "Controller.viewState.bundle";
+    public static final String KEY_VIEW_STATE_BUNDLE = "Controller.viewState.bundle";
     private static final String KEY_RETAIN_VIEW_MODE = "Controller.retainViewMode";
 
     private final Bundle args;
